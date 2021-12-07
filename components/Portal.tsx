6cf6a1edcf73e97d45aa4f6cfc65bf6dd2d9245d
jh0l <me@jh0l.com>
2021-12-07 21:12:58 +1100

    fix: :bug: make portals client side only to prevent missing document object

--- components/Portal.tsx
+++ components/Portal.tsx
@@ -1,27 +1,7 @@
-import {memo, ReactNode, useEffect, useRef} from 'react';
-import {createPortal} from 'react-dom';
+import dynamic from 'next/dynamic';
 
-function Portal({children}: {children: ReactNode}) {
-    /**
-     * keeps ref between renders
-     */
-    const el = useRef<HTMLDivElement | null>(null);
+const Portal = dynamic(() => import('./PortalBasic'), {
+    ssr: false,
+});
 
-    /**
-     * create element if empty (for the first time render only)
-     */
-    if (!el.current) el.current = document.createElement('div');
-
-    useEffect(() => {
-        const mount = document.getElementById('portal-root');
-        const {current} = el;
-        mount?.appendChild(current as Node);
-        return () => {
-            mount?.removeChild(current as Node);
-        };
-    }, []);
-
-    return createPortal(children, el.current);
-}
-
-export default memo(Portal);
+export default Portal;
---
+++ components/PortalBasic.tsx
@@ -0,0 +1,29 @@
+import {memo, ReactNode, useEffect, useRef} from 'react';
+import {createPortal} from 'react-dom';
+
+function Portal({children}: {children: ReactNode}) {
+    /**
+     * keeps ref between renders
+     */
+    const el = useRef<HTMLDivElement | null>(null);
+
+    /**
+     * create element if empty (for the first time render only)
+     */
+    if (!el.current) el.current = document.createElement('div');
+
+    useEffect(() => {
+        const mount = document.getElementById('portal-root');
+        const {current} = el;
+        mount?.appendChild(current as Node);
+        return () => {
+            mount?.removeChild(current as Node);
+        };
+    }, []);
+
+    return createPortal(children, el.current);
+}
+
+const PortalBasic = memo(Portal);
+
+export default PortalBasic;
